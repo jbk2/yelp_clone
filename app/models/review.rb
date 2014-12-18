fa750310e6a1475377387dd9c9337ee5608859ff
James B Kemp <jamesbkemp@me.com>
2014-12-18 12:39:57 +0000

    validating review model rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :restaurant
 
   validates :thoughts, presence: true, length: { minimum: 3 }
-  validates :rating, presence: true
+  validates :rating, presence: true, inclusion: { in: 1..5 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -8,12 +8,23 @@ describe Review do
 
   it 'is not valid without a rating' do
     review = Review.new(rating: nil)
-    expect(review).to have(1).errors_on(:rating)
+    expect(review).to have(2).errors_on(:rating)
   end
 
   it 'is not valid with a thoughts entry of <3 chars' do
     review = Review.new(thoughts: 'He')
     expect(review).to have(1).errors_on(:thoughts)
+  end
+
+  it 'is invalid if the rating is >5' do
+    review = Review.new(rating: 7)
+    expect(review).to have(1).errors_on(:rating)
+  end
+
+  it 'is invalid if the rating is <1' do
+    review = Review.new(rating: 0)
+    expect(review).to have(1).errors_on(:rating)
+
   end
 
 end
